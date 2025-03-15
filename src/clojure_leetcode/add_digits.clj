3c8b64781d448b0ef9f1e9ac0b5a85fc670b1a73
edubrunaldi <eduardo.brunaldi.santos@gmail.com>
2025-03-14 23:26:50 -0300

    add problem add_digits

---
+++ src/clojure_leetcode/add_digits.clj
@@ -0,0 +1,22 @@
+; https://leetcode.com/problems/add-digits
+(ns clojure-leetcode.add-digits)
+
+(defn one-digit?
+  [num]
+  (= (count (str num)) 1))
+
+(defn digits-sum
+  [num]
+  (reduce + (map Integer/parseInt (mapv str (str num)))))
+
+(defn add-digits
+  [num]
+  (if (one-digit? num)
+    num
+    (recur (digits-sum num))))
+
+; tests
+(assert (= 2 (add-digits 38)))
+(assert (= 0 (add-digits 0)))
+(assert (= 9 (add-digits 9)))
+(assert (= 7 (add-digits 1289347)))
